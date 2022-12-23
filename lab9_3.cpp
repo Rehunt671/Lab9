f483635edbc4034a13afdc5d0dd831cf496d04ea
dsfsd <dsfsd>
2022-12-23 15:21:58 +0700

    Success

--- lab9_3.cpp
+++ lab9_3.cpp
@@ -1,13 +1,36 @@
-/*
-"Enter your age: "
-"Enter your height: "
-"Enter your bounty: "
-"Your character = "
-"Zoro"
-"Sanji"
-"Usopp"
-"Chopper"
-"Franky"
-"Brook"
-"Jinbe"
-*/
+#include<iostream>
+#include<iomanip>
+#include<string>
+using namespace std;
+
+int age,h;
+long long int b;
+string c;
+
+int main(){
+   cout << "Enter your age: ";
+   cin >> age;
+   if(age <= 25){
+      cout << "Enter your height: ";
+      cin >> h;
+      if(h  < 100 ) c = "Chopper";
+      else if(h < 180) c = "Usopp";
+      else {
+         cout << "Enter your bounty: ";
+         cin >> b;
+         if(b > 1100000000) c = "Zoro";
+         else c = "Sanji";
+
+      }
+   }
+   else if(age <= 60) {
+      cout << "Enter your bounty: ";
+      cin >> b;
+      if(b > 500000000) c = "Jinbe";
+      else c = "Franky";
+
+   }
+   else if(age > 60) c = "Brook";
+   cout << "Your character = " << c;
+}
+
